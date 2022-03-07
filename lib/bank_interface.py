29a5560f9176d4dba54b5d2ba5bfc10c0aeddddd
Coombszy <LCCoombs@hotmail.co.uk>
2022-03-07 19:07:04 +0000

    Add move currency interface

--- lib/bank_interface.py
+++ lib/bank_interface.py
@@ -76,3 +76,35 @@ class Bank:
             self.logger.warn('Bank response was not Accepted')
             self.logger.warn(content)
         return int(content['response']['balance_sender'])
+    
+    def moveCurrency(self, user_id_sender: int, user_id_receiver: int, amount: int):
+        """Moves balance from one user id to another. Returns users balance afterwards as int"""
+        # Create payload/request for bank server
+        payload = dumps({
+            'action': 'moveCurrency',
+            'parameters': {
+                'user_id_sender': user_id_sender,
+                'user_id_receiver': user_id_receiver,
+                'amount': amount
+            }
+        })
+        # Create headers
+        headers = self.default_headers
+        headers['Content-Length'] = str(len(payload))
+        headers['Authorization'] = self.config['COMMS_SECRET']
+        # Send request
+        r = requests.post(url=self.config['COMMS_TARGET']+"/action",
+                data=payload,
+                headers=headers,
+                timeout=10
+            )
+        content = loads(r.content)
+        # If the code is not 200, raise an error with the response of the bank
+        if r.status_code != 200:
+            self.logger.warn('Bank response was a non 200')
+            self.logger.warn(content)
+            raise Exception(content)
+        if content['request'] != 'Accepted':
+            self.logger.warn('Bank response was not Accepted')
+            self.logger.warn(content)
+        return int(content['response'])
