a9fd4847f5792379e62b295209bbe0a4133df3b9
Coombszy <LCCoombs@hotmail.co.uk>
2022-03-08 00:23:54 +0000

    Add LICENSE

--- chameleon/main.py
+++ chameleon/main.py
@@ -80,3 +80,19 @@ async def command_nothing(ctx: commands.Context, *args):
 
 # Start the bot using TOKEN
 client.run(TOKEN)
+
+########################################################################################################
+#   Copyright (C) 2022  Liam Coombs
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
