6aeadad610165c4f2c25a56880e63bacd2cfa1b1
SamTipper <97979690+SamTipper@users.noreply.github.com>
2022-03-10 11:34:28 +0000

    New format for Bets JSON

--- gambling/bot.py
+++ gambling/bot.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import configparser
 import sys
 import discord
+import re
 
 
 sys.path.insert(0, '../')
@@ -17,63 +18,97 @@ db_schema = {
   "bets": []
 }
 
-#data = Database(logger, config['DATA_STORE'], db_schema)
 
 class Bot:
   def __init__(self, logger: Logger, config, bank, client: discord.Client):
-      self.logger = logger
-      self.config = config
-      self.client = client
-      self.bank = bank
-      self.guild_id = 0
-    
-  async def bet(self, ctx, args):
-    args = ' '.join(args)
-    if not len(args):
-      await ctx.send("poo brain")
+    self.logger = logger
+    self.config = config
+    self.client = client
+    self.bank = bank
+    self.data = Database(self.logger, self.config['DATA_STORE'], db_schema)
+
+  # Embed for bets
+  async def bet_embed(self, ctx, data):
+    embed = discord.Embed(
+      title='All Bets',
+      description='-------',
+      colour=discord.Colour.green()
+    )
+
+    for i in data["bets"]:
+      id = i['ID']
+      title = str(i['title'])
+      title = re.sub("[,'\[\]]", "", title)
+      wager = i['wagers']
+
+      complete = f"**Title:** \n {title.title()} \n" + f"**Bet Wager:** \n {wager}"
       
-    elif args[0] == "create":
+      embed.add_field(name=f'Bet ID: {id}', value=f'{complete}' ,inline=True)
+    await ctx.send(embed=embed)
+
+  # Bet command function
+  async def bet(self, ctx, args):
+    data = self.data.read()
+    args = list(args)
+    if not len(args):
+      await self.bet_embed(ctx, data)
+
+    elif args[0].lower() == "create":
       args.pop(0)
-      for word in args:
-        if word.isdigit():
-          pos = args.index(word)
-          wager = int(word)
-          args.pop(pos)
+      pro = {"users": [], "pool": 0}
+      against = {"users": [], "pool": 0}
+      await self.make_bet(ctx, args, pro, against)
+      args = str(args)
+      title = re.sub("[,'\[\]]", "", args)
+      await ctx.send(f"Bet \"{title.title()}\" created!")
+
+    elif args[0].lower() == "for":
+      user_id = ctx.message.author.id
+      args.pop(0)
+      ids_dump = []
+      for ids in data['ID']:
+        ids_dump.append(ids)
+      if user_id not in data['user_ids']:
+        if args[0] in ids_dump:
+          if args[1].isdigit():
+            wager = args[1]
+            answer = await self.check_and_spend(ctx, wager)
+            if answer == 0:
+              pass
           
-      user_balance = self.bank.getBalance(ctx.author.id)
-      # insufficient balance
-      if int(wager) >= user_balance:
-          await ctx.reply(f'Insufficient balance, current balance is {user_balance} VBC')
-          return
-      try:
-          self.bank.spendCurrency(ctx.author.id, wager)
-          await make_bet(ctx, args, wager)
-          
-      except Exception as e:
-          self.logger.warn('Failed to execute bet:')
-          self.logger.warn(str(e))
+  # Writing bet to DB
+  async def make_bet(self, ctx, args, pro, against):
+    data = self.data.read()
+    length = len(data["bets"])
+    while length in data["bets"]:
+      length += 1
+
+    wager_obj = {
+      "ID": length,
+      "title": args,
+      "for": pro,
+      "against": against,
+      "user_ids": []
+    }
+    
+    data["bets"].append(wager_obj)
+    self.data.write(data)
 
 
-async def make_bet(ctx, args, wager):
-  data1 = data.read()
-  print(data1)
+  async def check_and_spend(self, ctx, wager):
+    user_balance = self.bank.getBalance(ctx.author.id)
+    # Insufficient balance
+    if int(wager) >= user_balance:
+        await ctx.reply(f'Insufficient balance, current balance is {user_balance} VBC')
+        return
+    try:
+        self.bank.spendCurrency(ctx.author.id, wager)
+        return 0
+        
+    except Exception as e:
+        self.logger.warn('Failed to execute bet:')
+        self.logger.warn(str(e))      
 
-'''
-  balance = 0
-  for user in data['users']:
-      if user['user_id'] == user_id:
-          user['balance'] += amount
-          balance = user['balance']
-  self.data.write(data)
-  return balance
-
-
-
-# FIGURE THIS OUT LATER
-def user_id_exists(self, user_id: int):
-      """Does user name exist"""
-      data = self.data.read()
-      for user in data['users']:
-          if user['user_id'] == user_id:
-              return True
-      return False'''
+  
+  async def add_to_bet(self, ctx, choice):
+    pass
