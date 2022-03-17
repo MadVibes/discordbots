caf8016b9da4127a230a725ebd69bb9fca691cc3
SamTipper <rogoclock@gmail.com>
2022-03-17 01:40:05 +0000

    Updated Gambling

--- gambling/bot.py
+++ gambling/bot.py
@@ -62,19 +62,49 @@ class Bot:
       title = re.sub("[,'\[\]]", "", args)
       await ctx.send(f"Bet \"{title.title()}\" created!")
 
-    elif args[0].lower() == "for":
+    elif args[0].lower() == "for" or args[0].lower() == "against":
       user_id = ctx.message.author.id
+      choice = args[0]
       args.pop(0)
-      ids_dump = []
-      for ids in data['ID']:
-        ids_dump.append(ids)
-      if user_id not in data['user_ids']:
-        if args[0] in ids_dump:
-          if args[1].isdigit():
-            wager = args[1]
-            answer = await self.check_and_spend(ctx, wager)
-            if answer == 0:
-              pass
+      # Get bet dictionary where IDs match
+      active_bet = None
+      for bet in data['bets']:
+        if bet['ID'] == int(args[0]):
+          active_bet = bet
+          break
+
+      if active_bet is None:
+        await ctx.send("This bet doesn't exist, try again.")
+        await ctx.message.add_reaction('❌')
+          
+      if user_id not in active_bet['user_ids']:
+        if args[1].isdigit():
+          wager = int(args[1])
+          await self.check_and_spend(ctx, wager)
+          if choice.lower() == "for":
+            title = re.sub("[,'\[\]]", "", str(active_bet['title']))
+            active_bet['for']['pool'] += wager
+            users = active_bet['for']['users']
+            active_bet['user_ids'].append(user_id)
+            users.append(user_id)
+            self.data.write(data)
+            await ctx.send(f"You have bet {wager} VBC **FOR** {title}!")
+            await ctx.message.add_reaction('✅')
+
+          elif choice.lower() == "against":
+            title = re.sub("[,'\[\]]", "", str(active_bet['title']))
+            active_bet['against']['pool'] += wager
+            users = active_bet['against']['users']
+            active_bet['user_ids'].append(user_id)
+            users.append(user_id)
+            self.data.write(data)
+            await ctx.send(f"You have bet {wager} VBC **AGAINST** {title}!")
+            await ctx.message.add_reaction('✅')
+
+  async def pay(self, ctx, arg, arg2):
+    pass
+            
+          
           
   # Writing bet to DB
   async def make_bet(self, ctx, args, pro, against):
@@ -102,8 +132,7 @@ class Bot:
         await ctx.reply(f'Insufficient balance, current balance is {user_balance} VBC')
         return
     try:
-        self.bank.spendCurrency(ctx.author.id, wager)
-        return 0
+        self.bank.spendCurrency(ctx.author.id, int(wager))
         
     except Exception as e:
         self.logger.warn('Failed to execute bet:')
@@ -112,3 +141,19 @@ class Bot:
   
   async def add_to_bet(self, ctx, choice):
     pass
+
+########################################################################################################
+#   Copyright (C) 2022  Liam Coombs, Sam Tipper
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
